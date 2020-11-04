3e1d54bfb30ff38df722bafab11016a95d8b66c2
Iryna Prendkovych <iryna.prendkovych.itis.2020@lpnu.ua>
2020-11-04 20:26:24 +0200

    secound test

---
+++ лаб 5,6/UnitTest6/UnitTest6.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаб 5,6/лаб 5,6.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest6
+{
+	TEST_CLASS(UnitTest6)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			const double a = 1;
+			const double b = 2;
+			t = h(a, b);
+			Assert::AreEqual(t, 1.4);
+		}
+	};
+}
